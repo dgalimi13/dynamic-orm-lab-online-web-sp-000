2b2426606c0235db429bf95bddf27b697653e8d4
danny galimi <dannygalimi13@aim.com>
2021-02-24 20:38:02 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,7 +56,7 @@ class InteractiveRecord
   
   def self.find_by(hash) 
     binding.pry
-   if hash.keys[0].is_a? Integer
+
    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0]} = ? LIMIT 1" 
    DB[:conn].execute(sql, name, grade)
   end 
